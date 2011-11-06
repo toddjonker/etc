8a4efad401e1a1fe3bcb8c8e96ef77f0cd0f2f21
Todd V. Jonker <todd@toddjonker.net>
2011-11-05 19:53:35 -0700

    Added Perforce stuff to bash library.

---
+++ bash/p4.sh
@@ -0,0 +1,12 @@
+
+# This causes p4 to search for this file, where you should define things for
+# each client.  This avoids having trouble switching between depots in the
+# same shell.  The file should look like:
+#
+#  P4PORT=...
+#  P4CLIENT=...
+
+export P4CONFIG=.p4config
+
+
+alias p4-missing='find . -type f | xargs p4 files 2>&1 | perl -ne "/(\\S+) - no such file/ && print qq(\$1\\n)" | xargs p4 opened 2>&1 | grep "not opened" | cut -d" " -f1'
