8dfb27428fb01568e3f6336df746f11d68336f4e
Todd V. Jonker <todd@toddjonker.net>
2011-11-06 10:29:23 -0800

    Added p4.sh

--- bash/p4.sh
+++ bash/p4.sh
@@ -8,5 +8,7 @@
 
 export P4CONFIG=.p4config
 
+export P4EDITOR='emacs -nw'
+
 
 alias p4-missing='find . -type f | xargs p4 files 2>&1 | perl -ne "/(\\S+) - no such file/ && print qq(\$1\\n)" | xargs p4 opened 2>&1 | grep "not opened" | cut -d" " -f1'
