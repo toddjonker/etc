1a15629f4b76177558ff915a1b48ddf2e1e12d41
Todd V. Jonker <todd@toddjonker.net>
2011-11-10 15:49:48 -0800

    Merged common.sh into dotbashrc.sh; added declarative module loading.
    This allows the user to symlink from ~/.bashrc without modifications.

--- bash/dotbashrc.sh
+++ bash/dotbashrc.sh
@@ -1,6 +1,5 @@
-##  .bashrc
-##
-##  This file is loaded by each Bash subshell.
+##  This file should be symlinked from ~/.bashrc
+##  It is loaded by each Bash subshell.
 ##  See .bash_profile for code that is run only on first login.
 
 
@@ -8,26 +7,64 @@
 USER_LIBRARY=~/etc
 BASH_LIBRARY="${USER_LIBRARY}/bash"
 
-. "$BASH_LIBRARY/common.sh"
-
-# Host-specific stuff is loaded from:
-#    ~/.bashrc-localhost
-#    $BASH_LIBRARY/`hostname`.sh
-
-
-# Remove things you don't need:
-
-. "$BASH_LIBRARY/mac.sh"
-. "$BASH_LIBRARY/cygwin.sh"
-#. "$BASH_LIBRARY/amazon.sh"
-. "$BASH_LIBRARY/seedling.sh"
+if [ -e ~/bin ]
+then
+    PATH=$PATH:~/bin
+fi
+PATH=$PATH:$USER_LIBRARY/bin
 
 
-# More host-specific stuff can go here.
+shopt -u sourcepath     # Keep source and . commands from searching the PATH.
+shopt -s nocaseglob     # Case-insensitive filename completion
 
-#export JAVA_HOME=/cygdrive/c/java/j2sdk1.4.2_05
+
+if [ -n "$PS1" ]
+then
+    export PAGER=less
+    export LESS="-FQX"
+    export EDITOR=emacs
+fi
 
 
-cd ~
+##
+##  Host-specific stuff is loaded from the first readable file among:
+##
+##    ~/.bashrc-localhost
+##    $BASH_LIBRARY/`hostname`.sh
+##    $BASH_LIBRARY/local.sh
+
+# If you're sharing home directories between different platforms, you probably
+# should use HOSTNAME.sh instead of .bashrc-localhost
+
+# The loaded file should define USER_BASH_MODULES to list the modules to load.
+# But they will currently only be loaded for interactive shells.
+
+
+if [ -r ~/.bashrc-localhost ]
+then
+    . ~/.bashrc-localhost
+elif [ -r "$BASH_LIBRARY/`hostname`.sh" ]
+then
+    . "$BASH_LIBRARY/`hostname`.sh"
+elif [ -r "$BASH_LIBRARY/local.sh" ]
+then
+    . "$BASH_LIBRARY/local.sh"
+fi
+
+
+if [ -n "$PS1" ]
+then
+    for lib in ${USER_BASH_MODULES:-bindings prompt file ssh}
+    do
+	if [ -r "$BASH_LIBRARY/${lib}-local.sh" ]
+	then
+	    . "$BASH_LIBRARY/${lib}-local.sh"
+	else
+	    . "$BASH_LIBRARY/${lib}.sh"
+	fi
+    done
+fi
+unset USER_BASH_MODULES
+
 
 ## EOF ##
