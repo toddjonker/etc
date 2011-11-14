32ca5ecec284224c246df48fd675602feb50975d
Todd V. Jonker <todd@toddjonker.net>
2011-11-13 21:09:55 -0800

    Added bash/git.sh, loading git-completion

---
+++ bash/git.sh
@@ -0,0 +1,14 @@
+if [ -r /usr/local/git/contrib/completion/git-completion.bash ]
+then
+    # Read this file for documentation on this tool.
+    # I last read through it at git 1.7.7
+
+    source /usr/local/git/contrib/completion/git-completion.bash
+
+    GIT_PS1_SHOWDIRTYSTATE=1
+    GIT_PS1_SHOWSTASHSTATE=1
+    GIT_PS1_SHOWUNTRACKEDFILES=1
+    GIT_PS1_SHOWUPSTREAM=auto
+
+    PS1=$PS1_PREFIX'$(__git_ps1 " (%s)")'$PS1_SUFFIX
+fi
