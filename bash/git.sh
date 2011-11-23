031bda4cf7a9d3eacd35efa35c3c18a273f79d3f
Todd V. Jonker <todd@toddjonker.net>
2011-11-22 23:21:06 -0800

    GIT_CONTRIB can be separate from GIT_HOME. Unset both after use.

--- bash/git.sh
+++ bash/git.sh
@@ -1,11 +1,18 @@
-${GIT_HOME:=/usr/local/git}
+: ${GIT_HOME:=/usr/local/git}
+: ${GIT_CONTRIB:=$GIT_HOME/contrib}
 
-if [ -r "$GIT_HOME/contrib/completion/git-completion.bash" ]
+
+if [ -d /usr/local/git/bin ]
+then
+    PATH=$PATH:/usr/local/git/bin
+fi
+
+if [ -r "$GIT_CONTRIB/completion/git-completion.bash" ]
 then
     # Read this file for documentation on this tool.
     # I last read through it at git 1.7.7
 
-    source "$GIT_HOME/contrib/completion/git-completion.bash"
+    source "$GIT_CONTRIB/completion/git-completion.bash"
 
     GIT_PS1_SHOWDIRTYSTATE=1
     GIT_PS1_SHOWSTASHSTATE=1
@@ -14,3 +21,5 @@ then
 
     PS1=$PS1_PREFIX'$(__git_ps1 " (%s)")'$PS1_SUFFIX
 fi
+
+unset GIT_HOME GIT_CONTRIB
