c2432812732bc58f064cee5f97ea60543155cb08
Todd V. Jonker <todd@toddjonker.net>
2015-01-25 11:44:39 -0800

    Remove obsolete PATH entries.

--- bash/home-mac.sh
+++ bash/home-mac.sh
@@ -1,9 +1,8 @@
 #echo "default PATH=$PATH"
 
 declare -a baseBins=(
-    /usr/local/bin
 #    ~/bin
-    ~/dev/tools/maven-2.0.4/bin
+#    ~/dev/tools/maven-2.0.4/bin
 )
 
 for bin in "${baseBins[@]}"
@@ -15,9 +14,6 @@ do
 done
 
 unset baseBins
-
-# MacOS 10.6 has svn in /usr/bin
-PATH=/opt/subversion/bin:"$PATH"
 
 
 USER_BASH_MODULES="bindings prompt file ssh mac seedling git svn"
