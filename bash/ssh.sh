61b58a1dbe9f85e9068c1cc63c804d8750cdec53
Todd V. Jonker <todd@toddjonker.net>
2017-08-19 13:11:49 -0700

    SSH config uses RSA public key.
    According to GitHub, DSA keys were deprecated in OpenSSH 7.0.

--- bash/ssh.sh
+++ bash/ssh.sh
@@ -1,3 +1,3 @@
 # Start the SSH keychain. The first new terminal will request the passphrase.
-$USER_LIBRARY/bin/keychain --quiet ~/.ssh/id_dsa
+$USER_LIBRARY/bin/keychain --quiet ~/.ssh/id_rsa
 . ~/.ssh-agent-${HOSTNAME}
