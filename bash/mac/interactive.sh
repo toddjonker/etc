f18b1c5d410f38f82cb5b91e19bd77306c9024b3
Todd V. Jonker <todd@toddjonker.net>
2023-05-07 16:01:59 -0700

    Update homebrew completion setup.
    It still doesn't seem to work, though.

--- bash/mac/interactive.sh
+++ bash/mac/interactive.sh
@@ -15,16 +15,7 @@ else
     HOMEBREW_PREFIX=/usr/local
 fi
 
-if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
-    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
-else
-    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
-        if [[ -r "$COMPLETION" ]]; then
-            ss_log "Sourcing $COMPLETION"
-            source "$COMPLETION"
-        fi
-    done
-fi
+
 
 # We want to load (at least) git-prompt above before we get to the prompt setup.
 
@@ -48,4 +39,24 @@ then
 
     # https://iterm2.com/documentation-utilities.html
     test -d ~/.iterm2 && pathmunge ~/.iterm2 after
+fi
+
+
+# https://docs.brew.sh/Shell-Completion#configuring-completions-in-bash
+if type brew &>/dev/null
+then
+    HOMEBREW_PREFIX="$(brew --prefix)"
+    if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+    then
+	source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+    else
+	for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+	do
+	    if [[ -r "$COMPLETION" ]]
+	    then
+		ss_log "Sourcing $COMPLETION"
+		source "$COMPLETION"
+	    fi
+	done
+    fi
 fi
