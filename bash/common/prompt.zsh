f01f52a356bf09579c7b761ffc0475cfd151babf
Todd V. Jonker <todd@toddjonker.net>
2024-07-07 22:03:04 -0700

    Implement proper ZSH prompt, incuding Git.

--- bash/common/prompt.zsh
+++ bash/common/prompt.zsh
@@ -1,3 +1,35 @@
-# TODO Display Git status
 # TODO Display custom host label TVJ_DISPLAY_HOSTNAME
 # TODO Display custom title in Terminal window TERMINAL_WINDOW_TITLE
+
+PROMPT='%F{magenta}%3~ %#%f '
+
+
+# Add VCS info to the right prompt
+
+# Git provides more flexible mechanisms than ZSH, so let's try that.
+ss_load_modules git-prompt
+
+if [[ -n "$PS1_GIT_BITS" ]]
+then
+    # This is disabled by default since it messes with
+    # how the Bash prompt is colored.
+    GIT_PS1_SHOWCOLORHINTS=1
+else
+    # Fall-back to built-in ZSH functionality
+    # https://zsh.sourceforge.io/Doc/Release/User-Contributions.html#Version-Control-Information
+
+    autoload -Uz vcs_info
+    precmd_vcs_info() { vcs_info }
+    precmd_functions+=( precmd_vcs_info )
+
+    zstyle ':vcs_info:*' enable git
+    zstyle ':vcs_info:*' check-for-changes true
+    zstyle ':vcs_info:*' unstagedstr '*'                           # Output for %u
+    zstyle ':vcs_info:*' stagedstr   '+'                           # Output for %c
+    zstyle ':vcs_info:git:*' formats '%F{cyan}(%b%u%c)%%f'
+
+    PS1_GIT_BITS='${vcs_info_msg_0_}'
+fi
+
+setopt prompt_subst
+RPROMPT=$PS1_GIT_BITS
--- bash/mac/completion.zsh
+++ bash/mac/completion.zsh
@@ -1,7 +1,21 @@
 # https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
 if type brew &>/dev/null
 then
-  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+    HOMEBREW_PREFIX="$(brew --prefix)"
+
+    FPATH="${HOMEBREW_PREFIX}/share/zsh/site-functions:${FPATH}"
+
+    # Homebrew doesn't include this in site-functions!
+    GP="${HOMEBREW_PREFIX}/etc/bash_completion.d/git-prompt.sh"
+    if [[ -r "$GP" ]]
+    then
+        ss_log "Sourcing $GP"
+        source "$GP"
+    else
+        ss_log "No git-prompt.sh at $GP"
+    fi
+
+    unset GP HOMEBREW_PREFIX
 fi
 
 ss_source_next
