b656392f302466d6ce805304dceb2301a9658d5b
Todd V. Jonker <todd@toddjonker.net>
2024-08-15 14:52:24 -0700

    [zsh] Disable correct_all option, it's more annoying than helpful.

--- bash/common/options.zsh
+++ bash/common/options.zsh
@@ -1,5 +1,5 @@
 # Shell options for ZSH
 
-setopt auto_cd      # If unknown command is a directory, cd into it.
-setopt correct      # Autocorrect misspelled commands
-setopt correct_all  # Autocorrect misspelled arguments
+  setopt auto_cd      # If unknown command is a directory, cd into it.
+  setopt correct      # Autocorrect misspelled commands
+unsetopt correct_all  # Autocorrect misspelled arguments; very annoying
